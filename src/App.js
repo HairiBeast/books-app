f510fa28450b571d6449173a8f94033839bd2845
HairiBeast <shpetzla@gmail.com>
2022-10-05 17:59:48 +0100

    setup initial file structure, routes and basic css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import BookList from "./Components/BookList.jsx";
+import BookDetails from "./Components/BookDetails.jsx";
+import Navbar from "./Components/Navbar.jsx";
+import Footer from "./Components/Footer.jsx";
+import Favorites from "./Components/Favorites.jsx";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<BookList />} />
+        <Route path="/book/:id" element={<BookDetails />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/BookDetails.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "../App.css";
+
+const BookDetails = () => {
+  return <div>BookDetails</div>;
+};
+
+export default BookDetails;
