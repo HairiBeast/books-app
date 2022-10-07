a00f43c5e3e00e5d7eee0e00e55ed04cc58c077e
HairiBeast <shpetzla@gmail.com>
2022-10-07 15:42:36 +0100

    created context and wrapped around app for use with favorites functionality

--- src/Components/BookList.jsx
+++ src/Components/BookList.jsx
@@ -14,9 +14,25 @@ const BookList = () => {
         setBooks(res.data);
       })
       .catch((err) => console.log(err));
-  }, []);
+  });
 
-  return <div className="book-list"></div>;
+  return (
+    <div className="book-list">
+      {books.map((book) => (
+        <div className="book" key={book.id}>
+          <div>
+            <h4>{book.title}</h4>
+          </div>
+          <div>
+            <img src={book.image_url} alt="front cover" />
+          </div>
+          <div>
+            <button>Add to favourites</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default BookList;
