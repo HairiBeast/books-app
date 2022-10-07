b7dd574e9cd068e6630d1d785d281743d662c0ad
HairiBeast <shpetzla@gmail.com>
2022-10-07 15:14:29 +0100

    created a successful BookList API call

--- src/Components/BookList.jsx
+++ src/Components/BookList.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
+import axios from "axios";
 
 const BookList = () => {
-  return <div className="book-list">BookList</div>;
+  const API_URL = process.env.REACT_APP_API_URL;
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(API_URL)
+      .then((res) => {
+        console.log(res.data);
+        setBooks(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  return <div className="book-list"></div>;
 };
 
 export default BookList;
