72bba65fcd4a4ef6fa9ff156bb0c3cd17997a4fd
HairiBeast <shpetzla@gmail.com>
2022-10-17 01:32:39 +0100

    currently part way through tweaking styling to improve visibility

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -7,10 +7,12 @@ const Navbar = () => {
     <section className="hero">
       <h1>Library App</h1>
       <article>
-        <p>
-          Search for a range of books using DraftBit's API which responds with
-          the front-cover of a book and a selected quote.
-        </p>
+        <div className="paragraph-background">
+          <p>
+            Search for a range of books using DraftBit's API which responds with
+            the front-cover of a book and a selected quote.
+          </p>
+        </div>
         <Link to="/favorites">Favorites</Link>
       </article>
     </section>
